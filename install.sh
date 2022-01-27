bd8e7027813409f1edff89c359064e1a75990794
nhuyk56 <nhuyk56@gmail.com>
2022-01-27 08:58:21 +0700

    update

---
+++ install.sh
@@ -0,0 +1,57 @@
+git pull
+rm -rf node_modules
+rm -rf package.json
+npm i @babel/core@7.16.5 -D
+npm i @babel/plugin-proposal-class-properties@7.16.5 -D
+npm i @babel/preset-env@7.16.5 -D
+npm i @sampotts/eslint-config@.1.7 -D
+npm i autoprefixer@10.4.0 -D
+npm i aws-sdk@2.1046.0 -D
+npm i babel-eslint@10.1.0 -D
+npm i browser-sync@2.27.7 -D
+npm i colorette@.0.16 -D
+npm i cssnano@5.0.13 -D
+npm i del@6.0.0 -D
+npm i eslint@7.23.0 -D
+npm i fancy-log@1.3.3 -D
+npm i git-branch@2.0.1 -D
+npm i gulp@4.0.2 -D
+npm i gulp-awspublish@4.1.2 -D
+npm i gulp-better-rollup@4.0.1 -D
+npm i gulp-filter@7.0.0 -D
+npm i gulp-header@2.0.9 -D
+npm i gulp-hub@4.2.0 -D
+npm i gulp-if@3.0.0 -D
+npm i gulp-imagemin@7.1.0 -D
+npm i gulp-open@3.0.1 -D
+npm i gulp-plumber@1.2.1 -D
+npm i gulp-postcss@9.0.1 -D
+npm i gulp-rename@2.0.0 -D
+npm i gulp-replace@1.1.3 -D
+npm i gulp-sass@5.0.0 -D
+npm i gulp-size@4.0.1 -D
+npm i gulp-sourcemaps@3.0.0 -D
+npm i gulp-svgstore@9.0.0 -D
+npm i gulp-terser@2.1.0 -D
+npm i postcss@8.4.5 -D
+npm i postcss-custom-properties@12.0.1 -D
+npm i postcss-scss@4.0.2 -D
+npm i prettier-eslint@12.0.0 -D
+npm i prettier-stylelint@0.4.2 -D
+npm i remark-cli@10.0.1 -D
+npm i remark-validate-links@11.0.2 -D
+npm i rollup@2.61.1 -D
+npm i rollup-plugin-babel@4.4.0 -D
+npm i rollup-plugin-commonjs@10.1.0 -D
+npm i rollup-plugin-node-resolve@5.2.0 -D
+npm i sass@1.45.0 -D
+npm i stylelint@14.1.0 -D
+npm i stylelint-config-prettier@9.0.3 -D
+npm i stylelint-config-sass-guidelines@9.0.1 -D
+npm i stylelint-selector-bem-pattern@2.1.1 -D
+npm i core-js@3.20.0
+npm i custom-event-polyfill@1.0.7
+npm i loadjs@4.2.0
+npm i rangetouch@2.0.1
+npm i url-polyfill@1.1.12
+git checkout package.json
